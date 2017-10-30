13e154bcf00ece8d592976141327910bfedaaf30
System User <s6plwolf@cip09.physik.uni-bonn.de>
2017-10-30 18:01:26 +0100

    FIX: Loop correctly over lattice; remove redundant code; change hamiltonian

--- tutorials/tutorial_01/ising.py
+++ tutorials/tutorial_01/ising.py
@@ -12,38 +12,41 @@ class IsingLattice:
         self.lattice = lattice # store lattice if given, else None
         self.spins = spins if spins is not None else [1, -1]  # default spins are +-1
         self.j = j if j is not None else 1  # default j is 1
+        self.t = None  # Don't include temperature yet
         self.energy = 0
 
         self._init_lattice()
-        self._hamiltonian()
+        self.energy = self.hamiltonian(self.lattice, self.j, self.t)
 
     def _init_lattice(self):
 
         # lattice was provided
-        if self.lattice:
-            return
+        if isinstance(self.lattice, np.ndarray):
+            pass
+        # lattice was provided but is not np.array
+        elif not isinstance(self.lattice, np.ndarray) and self.lattice:
+            self.lattice = np.array(self.lattice)
+        # no lattice provided
         else:
             self.lattice = np.random.choice(self.spins, size=(self.n, self.n))
 
-    def _hamiltonian(self):
+    def hamiltonian(self, lattice, j, temp):
         """Calculate lattice hamiltonian"""
-        for i in range(self.n):
-            for j in range(self.n):
+        energy = 0
+        for i in range(lattice.shape[0]):
+            for j in range(lattice.shape[1]):
                 # Walk through lattice; avoid double counting by only going right and down.
                 # Nearest neighbour of border (i = self.n) is i=0.
-                # FIXME: This might still be incorrect
-                self.energy += self.lattice[i][j]*(self.lattice[(i+1) % self.n][j] + self.lattice[(i-1) % self.n][j])
-
-        # Multiply by self.j
-        self.energy *= -self.j
-
+                energy += lattice[i][j]*(lattice[(i+1) % lattice.shape[0]][j] + lattice[i][(j+1) % lattice.shape[1]])
+        
+        # Multiply by j
+        return j*energy
+        
+        #TODO: Use np.roll for hamiltonian
+        
     def rand_lattice(self):
         """Creates new random lattice"""
         self.lattice = np.random.choice(self.spins, size=(self.n, self.n))
-
-    def get_lattice(self):
-        """Return lattice"""
-        return self.lattice
 
     def show_lattice(self):
         """Show lattice config using matplotlib"""
@@ -52,6 +55,5 @@ class IsingLattice:
         plt.title('Configuration of %i x %i Ising lattice' % (self.n, self.n))
         plt.show()
 
-ising = IsingLattice(10)
-print ising.energy
+ising = IsingLattice(6)
 ising.show_lattice()
