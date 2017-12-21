22e2fbdf0bac27e431e8d0aa0847a635543cbe27
leloup314 <s6plwolf@uni-bonn.de>
2017-12-21 14:07:05 +0100

    ENH: rewrite, allow multi dimensional, Bak-Tang-Wiesenfeld approach, remove jitting

--- code/sandpile.py
+++ code/sandpile.py
@@ -1,100 +1,103 @@
+"""Implementation of the Bak-Tang-Wiesenfeld approach for cellular automation of sanpile dynamics"""
+
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 from mpl_toolkits.mplot3d import Axes3D  # Is used in background for 3D plotting
-from numba import njit  # use numbas njit for speed-up
+from numba import jit, njit, types  # use numbas njit for speed-up
 
-# Check abs(slope z)
-# Make function that returns slope
-# Look at slope in between sandpiles. If slope over z_crit, redistribute 1 grain to x,y with "highest" slope
-# Distribute randomly if ALL slopes are equal:
-# Drop only 1 grain vs drop until slope is < slope_crit
-
-# Markus git : flooklab
+sys.setrecursionlimit(10000)  # increase recursion depth to allow recursively add_sand for large lattice sites n
 
 
-def init_sandbox(n, state=None, crit_pile=4):
+def init_sandbox(n, dim, state=None, crit_pile=4):
     """
     Initialises an empty (0s) square NxN sandbox lattice on which the simulation is done.
 
-    :param n: int dimension of sandbox
+    :param n: int of lattice sites of sandbox
+    :param dim: int of dimension like n^dim
     :param state: str some state of the initial sandbox
     :param crit_pile: int critical height of pile of sand grains
 
     """
 
     if state == 'one':
-        res = np.ones(shape=(n, n), dtype=np.uint8)
+        res = np.ones(shape=(n, ) * dim, dtype=np.int16)
     elif state == 'crit':
-        res = np.full(shape=(n, n), fill_value=crit_pile - 1, dtype=np.uint8)
+        res = np.full(shape=(n, ) * dim, fill_value=crit_pile - 2, dtype=np.int16)
     elif state == 'over_crit':
-        res = np.full(shape=(n, n), fill_value=crit_pile + 1, dtype=np.uint8)
+        res = np.full(shape=(n, ) * dim, fill_value=crit_pile * 2, dtype=np.int16)
     else:  # None, ground state
-        res = np.zeros(shape=(n, n), dtype=np.uint8)
+        res = np.zeros(shape=(n, ) * dim, dtype=np.int16)
 
     return res
 
 
 @njit
-def off_boundary(s, x, y):
+def off_boundary(s, point):
     """
     Checks whether s[x][y] is beyond the boundary of the sandbox.
 
     :param s: np.array
-    :param x: int x coordinate of grain drop-off
-    :param y: int y coordinate of grain drop-off
+    :param point: tuple of coordinates of grain drop-off
     """
-    dims = (x, y)  # Non-mutable tuple increases speed-up ([500ns] vs (300ns))/loop
-    for i, dim in enumerate(dims):
+
+    for i, dim in enumerate(point):
         if dim > s.shape[i] - 1 or dim < 0:
             return True
     return False
 
 
-@njit
-def get_neighbours(x, y):
+#@njit
+def get_neighbours(point):
     """
-    Finds all neighbours of [x][y] and returns them
-    :param s: np.array
-    :param x: int x coordinate of grain drop-off
-    :param y: int y coordinate of grain drop-off
+    Finds all nearest neighbours of point with Euclidean distance == 1 and returns them
+    :param point: tuple of coordinates for which neighbours are returned
     :return: tuples with coordinates of neighbours
     """
-    return (x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)  # FIXME: hardcoded neighbours
+
+    nn = []
+    point = list(point)
+
+    for i, coordinate in enumerate(point):
+        for shift in (-1, 1):
+            nn.append(tuple(point[0: i] + [coordinate + shift] + point[i+1:]))
+
+    # For @njit use input np.array for neighbours
+    # for i in range(len(point)*2):
+        # for j, coordinate in enumerate(point):
+                # neighbour_array[i, j] = coordinate if i % len(point) == j else coordinate + 1 if i/len(point) < j else coordinate - 1
+
+    return nn
 
 
-@njit
-def add_sand(s, x, y, crit_pile):
+#@njit
+def add_sand(s, point, crit_pile):
     """
     Adds grain of sand at s[x][y] and checks criticality and boundary conditions.
 
     :param s: np.array
-    :param x: int x coordinate of grain drop-off
-    :param y: int y coordinate of grain drop-off
+    :param point: tuple of coordinates of grain drop-off
     :param crit_pile: int critical height of pile of sand grains
     """
 
     # If off-boundary, 'drop' the grain from the sandbox and do nothing
-    if not off_boundary(s, x, y):
+    if not off_boundary(s, point):
 
-        # Add grain to sandbox
-        s[x][y] += 1
+        # Add grain to sandbox; use numpys multi-indexing with tuples e.g. s[1][1] == s[(1, 1)] == s[1, 1]
+        s[point] += 1
 
         # Check if sandpile at s[x][y] is at critical level (or above, needed for over critical init)
-        if s[x][y] >= crit_pile:
+        if s[point] >= crit_pile:
 
-            actual_pile = s[x][y]  # Store actual height of pile to remove (can be > crit_pile for over_crit init)
-            s[x][y] = 0  # Remove all grains of sand from [x][y]
-            nearest_neighbours = get_neighbours(x, y)  # Get nearest neighbours of [x][y]
+            nearest_neighbours = get_neighbours(point)  # Get nearest neighbours of point
+            s[point] -= len(nearest_neighbours)   # Remove as many grains as neighbours and redistribute
 
-            # Redistribute grains in actual_pile by recursively calling add_sand for all neighbours of [x][y]
-            while actual_pile:
-                x_tmp, y_tmp = nearest_neighbours[actual_pile % len(nearest_neighbours)]
-                add_sand(s, x_tmp, y_tmp, crit_pile)
-                actual_pile -= 1
+            # Redistribute grains by recursively calling add_sand for all neighbours of point
+            for neighbour in nearest_neighbours:
+                add_sand(s, neighbour, crit_pile)
 
 
-@njit
 def add_sand_random(s, crit_pile):
     """
     Adds one grain of sand at a random place in the sandbox.
@@ -103,9 +106,13 @@ def add_sand_random(s, crit_pile):
     :param crit_pile: int critical height of pile of sand grains
     """
 
-    x_rand = np.random.randint(low=0, high=s.shape[0])
-    y_rand = np.random.randint(low=0, high=s.shape[1])
-    add_sand(s, x_rand, y_rand, crit_pile)
+    # Make random point
+    point = []
+    for dim in range(len(s.shape)):
+        point.append(np.random.randint(low=0, high=s.shape[dim]))
+    point = tuple(point)
+
+    add_sand(s, point, crit_pile)
 
 
 def plot3d(s, iterations, crit_pile):
@@ -153,15 +160,13 @@ def plot2d(s, iterations, crit_pile):
     img = plt.imshow(s, cmap='jet', vmin=0, vmax=crit_pile)  # make image with colormap BlueGreenRed
     plt.colorbar(img)  # add colorbar
 
-    for _ in range(iterations):
+    for i in range(iterations):
         add_sand_random(s, crit_pile)
-        img.set_data(s)  # update image
-        plt.pause(0.00001)  # pause to allow interactive plotting
+        img.set_data(s)
+        plt.pause(.000002)
 
     plt.ioff()
     plt.show()
-
-
 
 
 def main():
@@ -169,9 +174,12 @@ def main():
     # Init variables and sandbox
     iterations = 100
     crit_pile = 6
-    sandbox = init_sandbox(5, state='ground', crit_pile=crit_pile)
+    dim = 2
+    n = 5
+    sandbox = init_sandbox(n, dim=dim, state='ground', crit_pile=crit_pile)
+    # neighbour_array = np.zeros(shape=(dim*2, dim), dtype=np.int16)
 
-    plot2d(sandbox, iterations, crit_pile)
+    plot3d(sandbox, iterations, crit_pile)
 
 if __name__ == '__main__':
     main()
